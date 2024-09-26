1018491a3f73ddd73e438de23669092f070bd757
mr-seaking <pavelshuvalov@live.com>
2024-09-26 18:21:00 +0300

    1

--- HW/HW2_1/Program.cs
+++ HW/HW2_1/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Занятие 2. ДЗ 1:");
+﻿using System;
+
+Console.WriteLine("Занятие 2. ДЗ 1:");
 Console.WriteLine("Введите поочередно три целых числа");
 try
 {
@@ -15,9 +17,53 @@ try
     var length = nums.Length;
     var sum = n1 + n2 + n3;
     var res = sum / length;
-    Console.Write("среднее арифметическое значение всех введённых чисел: " + res);
+    Console.WriteLine("среднее арифметическое значение всех введённых чисел: " + res);
 }
 catch (FormatException)
 {
-    Console.Write("Вы ввели не число");
+    Console.WriteLine("Вы ввели не число");
+}
+Console.WriteLine("\n Занятие 2. ДЗ 2:");
+
+Console.WriteLine("С консоли вводятся данные о пасажирах: ФИО и номер места");
+try
+{
+    Console.Write("Введите ФИО: ");
+    string fio1 = Console.ReadLine();
+    Console.Write("Введите номер места: ");
+    string place1 = Console.ReadLine();
+
+    Console.Write("Введите ФИО: ");
+    string fio2 = Console.ReadLine();
+    Console.Write("Введите номер места: ");
+    string place2 = Console.ReadLine();
+
+    Console.Write("Введите ФИО: ");
+    string fio3 = Console.ReadLine();
+    Console.Write("Введите номер места: ");
+    string place3 = Console.ReadLine();
+
+    Console.Write("Введите ФИО: ");
+    string fio4 = Console.ReadLine();
+    Console.Write("Введите номер места: ");
+    string place4 = Console.ReadLine();
+
+    string[,] passengers = { { fio1, place1 }, { fio2, place2 }, { fio3, place3 }, { fio4, place4 } };
+
+    Console.Write("Номер пасажира: ");
+    int num = int.Parse(Console.ReadLine());
+    Console.WriteLine($"ФИО: {passengers[num - 1, 0]}" + "\n" + $"Место: {passengers[num - 1, 1]}");
+}
+
+catch (IndexOutOfRangeException)
+{
+    Console.WriteLine("Вы ввели не существующее место");
+}
+catch (FormatException)
+{
+    Console.WriteLine("Вы ввели не число");
+}
+finally
+{
+    Console.WriteLine("Счастилого пути!");
 }
